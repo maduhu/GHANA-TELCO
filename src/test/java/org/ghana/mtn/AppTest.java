f2e873f18823b27504568c8b81d369182760046c
Arvind <arvindc@thoughtworks.com>
2011-09-23 11:18:30 +0530

    Initial Reposiory create checkin

---
+++ src/test/java/org/ghana/mtn/AppTest.java
@@ -0,0 +1,13 @@
+package org.ghana.mtn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest
+{
+    @Test
+    public void ShouldCallInitialTest() {
+      assertTrue(true);
+    }
+}
