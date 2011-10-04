7daf05a427491442804f421df6d11f03f3aa29cc
Arvind Kumar C <arvindc@thoughtworks.com>
2011-10-04 11:48:54 +0530

    Billing Service Skeleton.

---
+++ ghana-mtn-billing/src/test/java/org/motechproject/ghana/mtn/billing/BillingServiceImplTest.java
@@ -0,0 +1,10 @@
+package org.motechproject.ghana.mtn.billing;
+
+import org.junit.Test;
+
+public class BillingServiceImplTest {
+    @Test
+    public void ShouldTest() {
+        BillingServiceImpl billingService = new BillingServiceImpl();
+    }
+}
