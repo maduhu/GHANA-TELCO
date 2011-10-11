06c3b360144d7239136e0b2320eb6212cbd44eec
vijayaraj <vijayaraj@yahoo.com>
2011-10-11 15:37:42 +0530

    #refactoring|589| adding tests for new subscription components <vijay>

--- ghana-mtn-web/src/test/java/org/motechproject/ghana/mtn/service/process/SubscriptionBillingTest.java
+++ ghana-mtn-web/src/test/java/org/motechproject/ghana/mtn/service/process/SubscriptionBillingTest.java
@@ -7,6 +7,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.motechproject.ghana.mtn.billing.dto.BillingCycleRequest;
 import org.motechproject.ghana.mtn.billing.dto.BillingServiceResponse;
+import org.motechproject.ghana.mtn.billing.dto.CustomerBill;
 import org.motechproject.ghana.mtn.billing.service.BillingService;
 import org.motechproject.ghana.mtn.domain.MessageBundle;
 import org.motechproject.ghana.mtn.domain.ProgramType;
@@ -19,9 +20,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
-
 
 public class SubscriptionBillingTest {
     private SubscriptionBilling billing;
@@ -32,7 +34,6 @@ public class SubscriptionBillingTest {
     @Mock
     private MessageBundle messageBundle;
 
-
     @Before
     public void setUp() {
         initMocks(this);
@@ -40,34 +41,120 @@ public class SubscriptionBillingTest {
     }
 
     @Test
-    public void shouldReturnFalseInCaseOfValidationErrors() {
+    public void shouldReturnFalseInCaseOfValidationErrorsOnStartingCycle() {
         DateTime now = DateTime.now();
         String mobileNumber = "123";
-        String errorMsg = "errors message";
+        String program = "program";
+
+        ProgramType programType = mock(ProgramType.class);
+        Subscription subscription = mock(Subscription.class);
+        BillingServiceResponse response = mock(BillingServiceResponse.class);
+        List errors = new ArrayList<ValidationError>();
+
+        setupMocks(now, mobileNumber, program, programType, subscription);
+
+        when(response.hasErrors()).thenReturn(true);
+        when(response.getValidationErrors()).thenReturn(errors);
+        when(messageBundle.get(errors)).thenReturn("errors message");
+        when(billingService.startBilling(any(BillingCycleRequest.class))).thenReturn(response);
+
+        Boolean reply = billing.startFor(subscription);
+
+        assertFalse(reply);
+        assertSMSRequest(mobileNumber, "errors message", program);
+    }
+
+    @Test
+    public void shouldReturnTrueAndSendBillingSuccessMessageOnStartingCycle() {
+        DateTime now = DateTime.now();
+        String mobileNumber = "123";
+        String program = "program";
+
+        ProgramType programType = mock(ProgramType.class);
+        Subscription subscription = mock(Subscription.class);
+        BillingServiceResponse response = mock(BillingServiceResponse.class);
+        CustomerBill customerBill = mock(CustomerBill.class);
+
+        setupMocks(now, mobileNumber, program, programType, subscription);
+
+        when(customerBill.amountCharged()).thenReturn(new Double(12));
+        when(response.hasErrors()).thenReturn(false);
+        when(response.getValue()).thenReturn(customerBill);
+        when(messageBundle.get(MessageBundle.BILLING_SUCCESS)).thenReturn("success message %s");
+        when(billingService.startBilling(any(BillingCycleRequest.class))).thenReturn(response);
+
+        Boolean reply = billing.startFor(subscription);
+
+        assertTrue(reply);
+        assertSMSRequest(mobileNumber, "success message 12.0", program);
+    }
+
+
+    @Test
+    public void shouldReturnFalseInCaseOfValidationErrorsOnStoppingCycle() {
+        DateTime now = DateTime.now();
+        String mobileNumber = "123";
+        String program = "program";
+
+        ProgramType programType = mock(ProgramType.class);
+        Subscription subscription = mock(Subscription.class);
+        BillingServiceResponse response = mock(BillingServiceResponse.class);
+        List errors = new ArrayList<ValidationError>();
+
+        setupMocks(now, mobileNumber, program, programType, subscription);
+
+        when(response.hasErrors()).thenReturn(true);
+        when(response.getValidationErrors()).thenReturn(errors);
+        when(messageBundle.get(errors)).thenReturn("errors message");
+        when(billingService.stopBilling(any(BillingCycleRequest.class))).thenReturn(response);
+
+        Boolean reply = billing.endFor(subscription);
+
+        assertFalse(reply);
+        assertSMSRequest(mobileNumber, "errors message", program);
+    }
+
+
+    @Test
+    public void shouldReturnTrueAndSendBillingSuccessMessageOnStoppingCycle() {
+        DateTime now = DateTime.now();
+        String mobileNumber = "123";
         String program = "program";
         ProgramType programType = mock(ProgramType.class);
         Subscription subscription = mock(Subscription.class);
-        List errors = new ArrayList<ValidationError>();
         BillingServiceResponse response = mock(BillingServiceResponse.class);
+        CustomerBill customerBill = mock(CustomerBill.class);
 
+
+        setupMocks(now, mobileNumber, program, programType, subscription);
+
+        when(customerBill.amountCharged()).thenReturn(new Double(12));
+        when(response.hasErrors()).thenReturn(false);
+        when(response.getValue()).thenReturn(customerBill);
+        when(messageBundle.get(MessageBundle.BILLING_STOPPED)).thenReturn("billing stopped");
+        when(billingService.stopBilling(any(BillingCycleRequest.class))).thenReturn(response);
+
+        Boolean reply = billing.endFor(subscription);
+
+        assertTrue(reply);
+        assertSMSRequest(mobileNumber, "billing stopped", program);
+    }
+
+    private void assertSMSRequest(String mobileNumber, String errorMsg, String program) {
+        ArgumentCaptor<SMSServiceRequest> captor = ArgumentCaptor.forClass(SMSServiceRequest.class);
+        verify(smsService).send(captor.capture());
+        SMSServiceRequest captured = captor.getValue();
+
+        assertEquals(errorMsg, captured.getMessage());
+        assertEquals(mobileNumber, captured.getMobileNumber());
+        assertEquals(program, captured.programName());
+    }
+
+    private void setupMocks(DateTime now, String mobileNumber, String program, ProgramType programType, Subscription subscription) {
         when(programType.getProgramName()).thenReturn(program);
         when(subscription.subscriberNumber()).thenReturn(mobileNumber);
         when(subscription.billingStartDate()).thenReturn(now);
         when(subscription.getProgramType()).thenReturn(programType);
-        when(response.hasErrors()).thenReturn(true);
-        when(response.getValidationErrors()).thenReturn(errors);
-        when(messageBundle.get(errors)).thenReturn(errorMsg);
-        when(billingService.startBilling(any(BillingCycleRequest.class))).thenReturn(response);
-
-        billing.startFor(subscription);
-
-        ArgumentCaptor<SMSServiceRequest>  captor = ArgumentCaptor.forClass(SMSServiceRequest.class);
-        verify(smsService).send(captor.capture());
-        SMSServiceRequest captured = captor.getValue();
-        assertEquals(errorMsg, captured.getMessage());
-        assertEquals(mobileNumber, captured.getMobileNumber());
-        assertEquals(program, captured.programName());
-
     }
 
 
