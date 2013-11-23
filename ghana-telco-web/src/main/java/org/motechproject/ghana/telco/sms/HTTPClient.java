5a3c74472d2a8c258c7c6515c15cfab1e1fad38e
David Hutchful <dhutchful@gmail.com>
2013-11-23 07:48:21 +0000

    Made modifications to enable connection with Kannel

---
+++ ghana-telco-web/src/main/java/org/motechproject/ghana/telco/sms/HTTPClient.java
@@ -0,0 +1,11 @@
+package org.motechproject.ghana.telco.sms;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dhutchful
+ * Date: 11/12/13
+ * Time: 3:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HTTPClient {
+}
